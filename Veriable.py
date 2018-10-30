185d0d01546893e764add064a61870703583c307
rhysmason <44345929+rhysmason@users.noreply.github.com>
2018-10-30 22:22:49 +0000

    Add files via upload

---
+++ Veriable.py
@@ -0,0 +1,47 @@
+# to use a veriable you need a word like hello
+# then you get the command you need like "Hello"
+# look
+
+hello = "Hello"
+
+print(hello)
+
+# you want to make a sentence you will need to do this
+# of corse put what ever you want 
+
+p1 = "How are you today? "
+p2 = "\n"
+
+# \n means new line
+
+p3 = "I am ok, you?"
+
+# then put the print  command
+
+print(p1, p2, p3)
+
+# the commer means a new veriable 
+# save then run it! 
+
+# Lets learn about the print command a bit more!
+# so a print command in python is basically when 
+# you want to print it to the consle (cmd or terminal)
+# you can put print("\n") to make a new line
+# you can also put plain text in there
+# by doing print("Hey how are you?")
+# here is an example:
+
+print("How was your day?", "\n", p3)
+
+# you can also add veriables!
+# now save and run it!
+# to disable a command you can put a
+# "#" in front of it like this;
+
+#print("Great, thanks for asking!")
+
+# now look!
+
+# this will be in the discription to download 
+# but thank you for watcing and see you later!
+# bye!
